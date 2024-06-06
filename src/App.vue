fc9df5684b679ba480035b30c0a026047ee460d1
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2024-06-05 19:52:16 -0500

    taller16

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Contador></Contador>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
