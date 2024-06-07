1f1f5c32dec9523b3c1daa4f3a590f14473a6891
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2024-06-06 19:50:07 -0500

    taller17

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img src="./assets/logo.png" alt="No existe">
+  <Contador titulo="Estudiante" />
+  <Contador titulo="Profesor" />
+  <Contador />
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
